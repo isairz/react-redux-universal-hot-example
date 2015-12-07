b7ffeae74feb379312505168ff17ab08d501b748
Seongjun Kim <isair.zun@gmail.com>
2015-12-07 08:17:42 -0500

    Temp Fix press api

--- api/actions/press.js
+++ api/actions/press.js
@@ -3,20 +3,10 @@ import Print from '../models/print';
 export default function press(req, params) {
   return new Promise((resolve, reject) => {
     const press = params[0];
-    if (!req.user) {
-      reject(new Error("Not Logged"));
-      return;
-    }
-    if (!req.user.username != params[0]) {
-      reject(new Error("Not Logged"));
-      return;
-    }
     Print.find({press: press}, (err, prints) => {
       console.log(prints);
       resolve({
-        username: req.user.username,
-        nickname: req.user.nickname,
-        cash: req.user.cash,
+        cash: 50000, // FIXME
         requested: prints.reverse().map(print => ({
           filename: print.originalName,
           path: print.path,
